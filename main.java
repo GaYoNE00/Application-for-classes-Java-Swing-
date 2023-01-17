4e04a2efa0166cd833699ea9556832ae91d2eb22
GaYoNE <91003766+GaYoNE00@users.noreply.github.com>
2023-01-17 23:19:39 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+	//	main_frame mf = new main_frame();
+	//	Loginform lf = new Loginform(1);
+	//	Student_select ss = new Student_select();
+	//	point pt = new point();
+//		student_option st = new student_option();
+//		study_list sl = new study_list();
+//		study_option so = new study_option();
+		main_frame mf = new main_frame();
+	}
+
+
+}
